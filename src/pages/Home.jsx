2359673b2f93aa005eb776e615710fc3456626ba
JEEVAABI <jeevaabishake@gmail.com>
2023-06-05 09:57:15 +0530

    Initial commit, with basic home page creation for web site.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Navbar from '../layout/Navebar';
+// import Announcement from '../layout/Announcement';
+// import Carousel from '../components/Carousel';
+// import Categories from '../components/Categories';
+// import Products from '../components/Products';
+// import Newsletter from '../components/Newsletter';
+//import Footer from '../layout/Footer';
+
+const Home = () => {
+  return (
+    <>
+  
+      <Navbar />
+     
+      
+    </>
+  );
+};
+
+export default Home;
